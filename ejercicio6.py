3a847cf231f38b45cefccd4947f6f1561ae8a591
omaresteban09 <0marestebangomezpuerta@gmail.com>
2024-06-22 16:49:24 -0500

    ejercicio.-5 resuelto

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,10 +1,9 @@
-
-6. ```python
-    x = 10
-    y = 5
-    if x > y:
+"python"
+x = 10
+y = 5
+if x > y:
         print("x es mayor que y")
-    elif x < y:
+elif x < y:
         print("y es mayor que x")
-    else:
+else:
         print("x es igual a y")
