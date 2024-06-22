d7d43a6439bb246ba544dc75b1ee82417093e8e2
omaresteban09 <0marestebangomezpuerta@gmail.com>
2024-06-22 17:10:47 -0500

    ejercicio-6 resuelto

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,5 +1,6 @@
- def calcular_area_rectangulo(base, altura):
-     area = base * altura
-     print("El área del rectángulo es: " + area)
+#calcular_area_rectangulo(base, altura):
+base = 5
+altura = 10
+area = base * altura
+print(area)
 
- calcular_area_rectangulo(5, 10)
